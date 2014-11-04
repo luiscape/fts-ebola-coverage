be225a10e7e2a81ba0a6dc9eecc44858f21fdbb7
Luis Capelo <luiscape@gmail.com>
2014-11-04 11:08:37 -0500

    adding SW notifications

--- code/scraper.R
+++ code/scraper.R
@@ -13,42 +13,57 @@ onSw <- function(a = T, b = 'tool/') {
 source(paste(onSw(), 'code/write_tables.R', sep=""))
 source(paste(onSw(), 'code/sw_status.R', sep=""))
 
-
-# Function to get a single indicator value from ScraperWiki
-getIndicator <- function(api_endpoint = NULL, indicator_code = NULL) {
-  # building sql query
-  sql_query = paste("select * from value where indID='", indicator_code, "'", sep="")
-  q = paste(api_endpoint, sql_query, sep = "")
+runScraper <- function() {
   
-  # getting and parsing the data
-  doc <- fromJSON(getURL(q))
-  cat('Fetching data | ')
-  for(i in 1:length(doc)) {
-   it <- unlist(doc[[i]])
-   if(i == 1) output <- it
-   else output <- rbind(output, it)
+  # Function to get a single indicator value from ScraperWiki
+  getIndicator <- function(api_endpoint = NULL, indicator_code = NULL) {
+    # building sql query
+    sql_query = paste("select * from value where indID='", indicator_code, "'", sep="")
+    q = paste(api_endpoint, sql_query, sep = "")
+    
+    # getting and parsing the data
+    doc <- fromJSON(getURL(q))
+    cat('Fetching data | ')
+    for(i in 1:length(doc)) {
+      it <- unlist(doc[[i]])
+      if(i == 1) output <- it
+      else output <- rbind(output, it)
+    }
+    
+    cat('done!\n')
+    # done
+    return(output)
   }
   
-  cat('done!\n')
-  # done
-  return(output)
+  # Data frame with single indicator
+  data <- getIndicator('https://ds-ec2.scraperwiki.com/m3gwddd/yhe8ykk1zhnnf04/sql?q=', 'CHD.FUN.142')
+  sub <- as.data.frame(data)
+  row.names(sub) <- NULL
+  
+  # cleaning
+  sub$dsID <- NULL
+  sub$region <- NULL
+  sub$is_number <- NULL
+  sub$source <- NULL
+  names(sub) <- c('CHD_Indicator_Code', 'Date', 'Value')
+  sub$Indicator_Name <- 'Ebola Virus Outbreak - Overview of Needs and Requirements (inter-agency plan for Guinea, Liberia, Sierra Leone, Region) (Coverage)'
+  sub <- sub[sort(sub$Date, decreasing = T), ]
+  
+  
+  ## Writing output
+  write.csv(sub, paste(onSw(), 'http/fts-ebola-coverage.csv', sep=""), row.names = F)
+  writeTables(sub, "fts_ebola", "scraperwiki")
+
 }
 
-# Data frame with single indicator
-data <- getIndicator('https://ds-ec2.scraperwiki.com/m3gwddd/yhe8ykk1zhnnf04/sql?q=', 'CHD.FUN.142')
-sub <- as.data.frame(data)
-row.names(sub) <- NULL
-
-# cleaning
-sub$dsID <- NULL
-sub$region <- NULL
-sub$is_number <- NULL
-sub$source <- NULL
-names(sub) <- c('CHD_Indicator_Code', 'Date', 'Value')
-sub$Indicator_Name <- 'Ebola Virus Outbreak - Overview of Needs and Requirements (inter-agency plan for Guinea, Liberia, Sierra Leone, Region) (Coverage)'
-sub <- sub[sort(sub$Date, decreasing = T), ]
-
-
-## Writing output
-write.csv(sub, paste(onSw(), 'http/fts-ebola-coverage.csv', sep=""), row.names = F)
-
+# Changing the status of SW.
+tryCatch(runScraper(),
+         error = function(e) {
+           cat('Error detected ... sending notification.')
+           system('mail -s "FTS Ebola Coverage failed." luiscape@gmail.com')
+           changeSwStatus(type = "error", message = "Scraper failed.")
+{ stop("!!") }
+         }
+)
+# If success:
+changeSwStatus(type = 'ok')
