7983d77c1bc252d0a3ebddcc4472382d864f4168
Luis Capelo <luiscape@gmail.com>
2014-11-10 14:57:05 -0500

    adding a datastore sync script

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+
+# running the R scraper from the
+# virtual environment
 ~/R/R-3.1.1/bin/Rscript tool/code/scraper.R
+
+# running the python datastore scripts
+source ~/venv/bin/activate
+python ~/tool/code/create-datastore.py 93b92803-f9fa-45f4-bf72-73a8ab1d8922 API_KEY
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# running datastore tests from
+# python's virtual environment
+source ~/venv/bin/activate
+python ~/tool/tests/tests.py 93b92803-f9fa-45f4-bf72-73a8ab1d8922 API_KEY
