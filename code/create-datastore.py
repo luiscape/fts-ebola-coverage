71fe8ff78880f48bdc62d6104d6733c92bf30542
Luis Capelo <luiscape@gmail.com>
2014-11-28 15:15:51 -0500

    adding the sqlite function to the save_var sw function

--- code/create-datastore.py
+++ code/create-datastore.py
@@ -48,11 +48,11 @@ def checkHash(filename, first_run):
     # checking if the files are identical or if
     # they have changed
     if first_run:
-        scraperwiki.save_var('hash', new_hash)
+        scraperwiki.sqlite.save_var('hash', new_hash)
         new_data = False
 
     else:
-        old_hash = scraperwiki.get_var('hash')
+        old_hash = scraperwiki.sqlite.get_var('hash')
         scraperwiki.save_var('hash', new_hash)
         new_data = old_hash == new_hash
 
